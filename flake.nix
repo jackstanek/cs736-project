fecb6b6ec36255c885c814b2e56c0bdf504c2727
Jack Stanek <stanekjack@gmail.com>
2024-12-03 23:53:46 -0600

    Use clang 19, fix clangd compile_commands.json generation

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
       devShells = forAllSystems ({ pkgs }: {
         default = (pkgs.mkShell.override { stdenv = pkgs.llvmPackages_19.stdenv; }) {
           # The Nix packages provided in the environment
-          packages = with pkgs; [clang_18 cmake];
+          packages = with pkgs; [clang_19 cmake];
         };
       });
       packages = forAllSystems ({ pkgs }: {
