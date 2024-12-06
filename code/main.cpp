dfb197061db1d377b3fc1eaef1682e9ecc146e9a
Simon Frisk <simon@simonfrisk.com>
2024-12-05 20:05:00 -0600

    Sample mrc with regular time intervals

--- code/main.cpp
+++ code/main.cpp
@@ -9,6 +9,9 @@
 #include <csv.hpp>
 #include <gcache/ghost_kv_cache.h>
 
+// Time interval for MRC sampling
+const int timeDelta = 10;
+
 struct TraceReq {
     int timeStamp;
     std::string key;
@@ -29,16 +32,15 @@ void printTraceReq(TraceReq trace) {
 
 void saveMRCToFile(std::vector<std::tuple<uint32_t, uint32_t, gcache::CacheStat>> curve, std::string name) {
     std::ofstream file(std::format("mrc/{}.txt", name));
-
     for (auto& point : curve) {
-        file << std::get<2>(point).get_hit_rate() << std::get<0>(point) << " " << std::get<1>(point) << std::get<2>(point) << std::endl;
+        file << std::get<2>(point).get_hit_rate() << " " << std::get<0>(point) << " " << std::get<1>(point) << " " << std::get<2>(point) << std::endl;
     }
 
     file.close();
 }
 
 void initializeNewClientInGhost(int client, std::unordered_map<int, std::unique_ptr<gcache::SampledGhostKvCache<>>>& clientsGhostMap) {
-    clientsGhostMap[client] = std::make_unique<gcache::SampledGhostKvCache<5>>(1024*64,1024*64,1024*1024);
+    clientsGhostMap[client] = std::make_unique<gcache::SampledGhostKvCache<5>>(64,64,1024);
 }
 
 int main() {
@@ -47,33 +49,40 @@ int main() {
 
     std::unordered_map<int, std::unique_ptr<gcache::SampledGhostKvCache<>>> clientsGhostMap;
 
+    int saveTs = 0;
+
     for (csv::CSVRow& row : reader) {
+
         if (row[0].is_int() && row[2].is_int() && row[3].is_int() && row[4].is_int()) {
+
             TraceReq req;
             req.key = row[1].get<std::string>();
             req.operation = row[5].get<std::string>();
-
             req.timeStamp = row[0].get<int>();
             req.keySize = row[2].get<int>();
             req.valSize = row[3].get<int>();
-            req.client = row[4].get<int>();
+            req.client = row[4].get<int>() % 32;
 
-            printTraceReq(req);
+            // Save the MRC curves for each client with a certain time interval
+            if(req.timeStamp - saveTs > timeDelta) {
+                saveTs = (req.timeStamp / timeDelta) * timeDelta;
+                std::cout << "TS " << saveTs << std::endl;
+                for (auto& kv : clientsGhostMap) {
+                    auto curve = kv.second->get_cache_stat_curve();
+                    saveMRCToFile(curve, std::format("{}_{}", std::to_string(kv.first), std::to_string(saveTs)));
+                }
+            }
+
+            // printTraceReq(req);
 
             if(clientsGhostMap.find(req.client) == clientsGhostMap.end()) {
                 initializeNewClientInGhost(req.client, clientsGhostMap);
             }
-
             clientsGhostMap[req.client]->access(req.key, req.keySize + req.valSize);
         }
     }
 
     file.close();
-
-    for (auto& kv : clientsGhostMap) {
-        auto curve = kv.second->get_cache_stat_curve();
-        saveMRCToFile(curve, std::to_string(kv.first));
-    }
 
     return 0;
 }
